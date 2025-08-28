92ed86959a43e6ddbb3572f784c0a148b3198a22
RushiChaganti <rushi2004rushi@gmail.com>
2025-08-28 11:08:54 +0530

    V 2.3
    mend

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   basePath: '/NeuralSim',
+  output: 'export',
 };
 
 export default nextConfig;
