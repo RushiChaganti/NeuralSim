3cda9ae638403d8552d8e8d38c48c881c432c231
RushiChaganti <rushi2004rushi@gmail.com>
2025-08-28 17:45:40 +0530

    V 3.0
    mend

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // replaces next export
+  output: "export",
+ basePath: "/neuralsim",
+ assetPrefix: "/neuralsim/",
+  images: {
+    unoptimized: true,           // needed for next/image in static export
+  },
 };
 
 module.exports = nextConfig;
